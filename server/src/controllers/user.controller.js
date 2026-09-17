3d37cac5f72c54423229442f918dbe3d5fac22ea
Arsh <arshahmad235@gmail.com>
2026-09-17 10:24:38 +0530

    feat: signin routes added

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import { User, Task } from "../models/models.js";
-import { UserSchema, TaskSchema } from "../types/types.js";
+import { userSignUp, userSignIn, TaskSchema } from "../types/types.js";
 
 export const UserRegister = async (req, res) => {
    try {
-      const success = UserSchema.safeParse(req.body);
+      const success = userSignUp. userSignIn,safeParse(req.body);
 
       if (!success.success) {
          return res.status(400).json({
@@ -60,5 +60,64 @@ export const UserRegister = async (req, res) => {
          success: false,
          message: "User registration failed",
       });
+   }
+};
+
+export const UserSignin = async (req, res) => {
+   try {
+      const result = userSignIn.safeParse(req.body);
+
+      if(!result.success){
+            return res.status(400).json({
+            success: false,
+            message: "Invalid user credentials",
+            errors: result.error,
+         });
+      }
+
+      const {email, password} = result.data;
+
+      const user = await User.findOne({email}).lean();
+
+      if(!user){
+         return res.status(400).json({
+            success: false,
+            message: "User doesn't exist with this email"
+         })
+      }
+
+      const isMatch = await User.isPasswordCorrect(password);
+      if(!isMatch){
+         return res.status(401).json({
+            success: false,
+            message: "Invalid email or password"
+         })
+      }
+
+      const token = jwt.sign({id: user._id}, process.env.SECRET_KEY, {expiresIn: "24h"});
+
+      res.cookie("token", token, {
+         httpOnly: true,
+         secure: process.env.NODE_ENV === "production",
+         sameSite: "strict",
+         maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      res.setHeader("Authorization", `Bearer ${token}`);
+
+      return res.status(200).json({
+         message: "Signin successful",
+         user: {
+            id: user._id,
+            email: user.email,
+         },
+      });
+
+   } catch (error) {
+      return res.status(500).json({
+         success: false,
+         message: "Internal server error"
+      })
+
    }
 };
--- server/src/types/types.js
+++ server/src/types/types.js
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
-export const UserSchema = z.object({
+export const userSignUp = z.object({
    username: z.string().min(3),
+   email: z.email(),
+   password: z.string().min(6).max(8),
+});
+
+export const userSignIn = z.object({
    email: z.email(),
    password: z.string().min(6).max(8),
 });
